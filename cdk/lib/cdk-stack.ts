2f677a415d8b573e75a402faa37ddda556609ed7
Yusuke Sakai <yus_sakai@yumemi.co.jp>
2023-08-19 09:15:10 +0900

    feat: S3 イベント通知

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -2,20 +2,27 @@ import {
   aws_ec2 as ec2,
   aws_ecr as ecr,
   aws_ecs as ecs,
-  aws_iam as iam,
-  aws_scheduler as scheduler,
-  aws_sqs as sqs,
+  aws_s3 as s3,
   aws_stepfunctions as sfn,
   aws_stepfunctions_tasks as tasks,
+  aws_events as events,
+  aws_events_targets as events_targets,
   Stack,
   StackProps,
 } from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { TaskInput } from "aws-cdk-lib/aws-stepfunctions";
 
 export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const ecrRepository = process.env.ECR_REPOSITORY ?? "";
+    const bucketName = process.env.BUCKET_NAME ?? "";
+    if (ecrRepository === "" || bucketName === "") {
+      throw new Error(
+        "環境変数 ECR_REPOSITORY または BUCKET_NAME が設定されていない",
+      );
+    }
 
     // VPC
     const vpc = new ec2.Vpc(this, "Vpc", { maxAzs: 2, natGateways: 1 });
@@ -38,11 +45,7 @@ export class CdkStack extends Stack {
     );
     const goBatchContainer = taskDefinition.addContainer("goBatchContainer", {
       image: ecs.ContainerImage.fromEcrRepository(
-        ecr.Repository.fromRepositoryName(
-          this,
-          "ecrRepository",
-          "yus_sakai_test",
-        ),
+        ecr.Repository.fromRepositoryName(this, "ecrRepository", ecrRepository),
       ),
       environment: {
         ENV: "Original env value.",
@@ -52,123 +55,55 @@ export class CdkStack extends Stack {
       }),
     });
 
-    const containerOverrides1: tasks.ContainerOverride[] = [
+    const containerOverrides: tasks.ContainerOverride[] = [
       {
         containerDefinition: goBatchContainer,
-        command: sfn.JsonPath.listAt("$.commands"),
-        environment: [{ name: "ENV", value: sfn.JsonPath.stringAt("$.env") }],
+        command: sfn.JsonPath.array(
+          sfn.JsonPath.stringAt("$.detail.object.key"),
+        ) as any,
       },
     ];
-    const containerOverrides2: tasks.ContainerOverride[] = [
-      {
-        containerDefinition: goBatchContainer,
-        command: sfn.JsonPath.listAt("$.commands"),
-        environment: [{ name: "ENV", value: "From step functions2." }],
-      },
-    ];
-    const ecsRunTask1 = new tasks.EcsRunTask(this, "ecsRunTask1", {
+
+    const ecsRunTask = new tasks.EcsRunTask(this, "ecsRunTask", {
       integrationPattern: sfn.IntegrationPattern.RUN_JOB,
       cluster,
       taskDefinition,
-      containerOverrides: containerOverrides1,
-      launchTarget: new tasks.EcsFargateLaunchTarget(),
-      securityGroups: [runTaskSecurityGroup],
-    });
-    const ecsRunTask2 = new tasks.EcsRunTask(this, "ecsRunTask2", {
-      integrationPattern: sfn.IntegrationPattern.RUN_JOB,
-      cluster,
-      taskDefinition,
-      containerOverrides: containerOverrides2,
+      containerOverrides: containerOverrides,
       launchTarget: new tasks.EcsFargateLaunchTarget(),
       securityGroups: [runTaskSecurityGroup],
     });
 
     // see: https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_stepfunctions-readme.html
-    const execEcsRunStateMachine1 = new sfn.StateMachine(
+    const execEcsRunStateMachine = new sfn.StateMachine(
       this,
-      "execEcsRunStateMachine1",
+      "execEcsRunStateMachine",
       {
-        stateMachineName: "execEcsRunStateMachine1",
-        definitionBody: sfn.DefinitionBody.fromChainable(ecsRunTask1),
-      },
-    );
-    const execEcsRunStateMachine2 = new sfn.StateMachine(
-      this,
-      "execEcsRunStateMachine2",
-      {
-        stateMachineName: "execEcsRunStateMachine2",
-        definitionBody: sfn.DefinitionBody.fromChainable(ecsRunTask2),
+        stateMachineName: "execEcsRunStateMachine",
+        definitionBody: sfn.DefinitionBody.fromChainable(ecsRunTask),
       },
     );
 
-    // see: https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_stepfunctions_tasks-readme.html#step-functions
-    const stepFunctionsRunTask = new tasks.StepFunctionsStartExecution(
-      this,
-      "stepFunctionsRunTask1",
-      {
-        stateMachine: execEcsRunStateMachine1,
-        integrationPattern: sfn.IntegrationPattern.RUN_JOB,
-        input: TaskInput.fromObject({
-          commands: ["from1", "object1"],
-          env: "Start Execution input1",
-        }),
-      },
-    ).next(
-      new tasks.StepFunctionsStartExecution(this, "stepFunctionsRunTask2", {
-        stateMachine: execEcsRunStateMachine2,
-        integrationPattern: sfn.IntegrationPattern.RUN_JOB,
-        input: TaskInput.fromObject({
-          commands: ["from2", "object2"],
-          env: "Start Execution input2",
-        }),
-      }),
-    );
-
-    const execStepFunctionsStateMachine = new sfn.StateMachine(
-      this,
-      "execStepFunctionsStateMachine",
-      {
-        stateMachineName: "execStepFunctionsStateMachine",
-        definitionBody: sfn.DefinitionBody.fromChainable(stepFunctionsRunTask),
-      },
-    );
-
-    // see: https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_iam-readme.html
-    const eventSchedulerRole = new iam.Role(this, "eventSchedulerRole", {
-      assumedBy: new iam.ServicePrincipal("scheduler.amazonaws.com"),
+    const bucket = new s3.Bucket(this, "eventBucket", {
+      bucketName: `${this.account}-${bucketName}`,
+      eventBridgeEnabled: true,
     });
-    eventSchedulerRole.addToPolicy(
-      new iam.PolicyStatement({
-        resources: ["*"],
-        actions: ["states:StartExecution"],
-      }),
-    );
 
-    const eventSchedulerDlq = new sqs.Queue(this, "EventSchedulerDlq", {
-      queueName: "event-scheduler-dlq",
-      encryption: sqs.QueueEncryption.KMS_MANAGED,
-    });
-    eventSchedulerDlq.grantSendMessages(eventSchedulerRole);
-
-    // see: https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_scheduler-readme.html
-    // see: https://docs.aws.amazon.com/ja_jp/AWSCloudFormation/latest/UserGuide/aws-resource-scheduler-schedule.html
-    new scheduler.CfnSchedule(this, `execStepFunctionsSchedule`, {
-      scheduleExpression: "cron(0 10 * * ? *)",
-      scheduleExpressionTimezone: "Asia/Tokyo",
-      flexibleTimeWindow: { mode: "OFF" },
-      state: "DISABLED",
-      target: {
-        arn: execStepFunctionsStateMachine.stateMachineArn,
-        roleArn: eventSchedulerRole.roleArn,
-        // see: https://docs.aws.amazon.com/ja_jp/eventbridge/latest/userguide/eb-rule-dlq.html
-        retryPolicy: {
-          maximumRetryAttempts: 0,
-        },
-        deadLetterConfig: {
-          arn: eventSchedulerDlq.queueArn,
+    new events.Rule(this, "S3EventRule", {
+      eventPattern: {
+        source: ["aws.s3"],
+        account: [this.account],
+        region: [this.region],
+        detailType: events.Match.equalsIgnoreCase("object created"),
+        detail: {
+          bucket: {
+            name: [bucket.bucketName],
+          },
+          object: {
+            key: [{ prefix: "target/" }],
+          },
         },
       },
-      groupName: "default",
+      targets: [new events_targets.SfnStateMachine(execEcsRunStateMachine)],
     });
   }
 }
